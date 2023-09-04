b2b3750797fc4180b8d88e4c14abbe6b8d8f6d85
gustavocotalima <gustavocotalima@hotmail.com>
2023-09-04 14:44:38 -0300

    get transaction and list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,31 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select('*')
+      .where({ id })
+      .first()
+
+    if (!transaction) {
+      return { error: 'Transaction not found' }
+    }
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionSchema = z.object({
       title: z.string(),
